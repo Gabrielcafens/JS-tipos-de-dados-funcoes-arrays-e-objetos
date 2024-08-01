3d08addfb0bacb5d3bd81027394ee8e9631366a0
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-01 18:52:19 -0300

    feat: array //concat junta os arrays sem alterar os originais das consts  salas de JavaScript e Python em juncaosalas

---
+++ arrays/juntar-salas.js
@@ -0,0 +1,6 @@
+const salaJS = ['Evaldo', 'Camis', 'Mari'];
+const salaPython = ['Ju', 'Leo', 'Raquel'];
+
+const juncaoSalas = salaJS.concat(salaPython);
+//concat junta os arrays sem alterar os originais
+console.log(juncaoSalas);
