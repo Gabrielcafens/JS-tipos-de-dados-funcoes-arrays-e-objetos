bc1705a0707983c27a8c7aff59f32ac46a3c5bb8
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-01 18:25:32 -0300

    feat: fixmedia

--- funcoes.js
+++ funcoes.js
@@ -4,5 +4,6 @@
 function exibirInfosEstudante(nome, nota) {
   return `o nome do estudante é ${nome} e a nota é ${nota}`;
 }
+
 console.log(exibirInfosEstudante('Café com Java', 10));
 console.log(exibirInfosEstudante('Café', 7));
