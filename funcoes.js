66a56c949246dd9a2875e98aacfad804f4a98c90
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-01 17:22:50 -0300

    feat: Adicionar função para exibir informações do estudante  A função `exibirInfosEstudante` foi adicionada ao arquivo `funcoes.js` para exibir o nome e a nota do estudante. :+1:

---
+++ funcoes.js
@@ -0,0 +1,8 @@
+//parametros/argumentos
+//retorno
+
+function exibirInfosEstudante(nome, nota) {
+  return `o nome do estudante é ${nome} e a nota é ${nota}`;
+}
+console.log(exibirInfosEstudante('Café com Java', 10));
+console.log(exibirInfosEstudante('Café', 7));
