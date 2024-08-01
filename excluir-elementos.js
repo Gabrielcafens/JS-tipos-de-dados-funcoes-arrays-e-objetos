0a95131a90cf11201a0d7b8967f0c9bccdc170d7
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-01 18:31:36 -0300

    feat: Remover último elemento do array e calcular média das notas

---
+++ excluir-elementos.js
@@ -0,0 +1,7 @@
+//metodo pop remove o ultimo elemento do array
+
+const notas = [10, 6, 8, 5.5, 10];
+notas.pop();
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media.toFixed(2));
