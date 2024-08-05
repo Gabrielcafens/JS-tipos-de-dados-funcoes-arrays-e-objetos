b9fdfa6f24f5ad0c95f2d4b36d125eea982cc2e2
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-05 16:14:04 -0300

    Refatorar código para adicionar comentários explicativos no arquivo de estrutura de objeto

---
+++ objetos/alterando-objeto.js
@@ -0,0 +1,14 @@
+const estudante = {
+  nome: 'José Silva',
+  idade: 32,
+  cpf: '12312312312',
+  turma: 'JavaScript'
+}
+console.log(estudante);
+
+estudante.telefone = '551199999999';
+console.log(estudante.telefone);
+
+console.log(estudante);
+console.log(`o nome de estudante é ${estudante.nome}`);
+
