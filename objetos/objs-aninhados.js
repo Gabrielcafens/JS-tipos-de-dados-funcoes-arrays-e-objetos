e93eda817648bed91ba155e6aa027c0d81bc2e88
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-05 16:24:26 -0300

    feat: Adicionar propriedade de endereço ao objeto estudante

---
+++ objetos/objs-aninhados.js
@@ -0,0 +1,16 @@
+const estudante = {
+  nome: "José Silva",
+  idade: 32,
+  cpf: "12312312312",
+  turma: "JavaScript",
+  bolsista: true,
+  telefones: ["81988776652","8198872122"]
+
+};
+
+estudante.endereco = {
+  rua: "Av. Paulista",
+  numero: "123",
+  complemento: "apt 123"
+}
+console.log(estudante.endereco.rua);
