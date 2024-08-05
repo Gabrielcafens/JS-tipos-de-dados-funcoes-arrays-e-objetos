52b9ae9e3898e3c93b9487c8481cfd9d23dfe684
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-05 16:18:20 -0300

    feat: Adicionar arquivo de dados e valores do estudante em arry

---
+++ objetos/dados-valores.js
@@ -0,0 +1,12 @@
+const estudante = {
+  nome: "José Silva",
+  idade: 32,
+  cpf: "12312312312",
+  turma: "JavaScript",
+  bolsista: true,
+  telefones: ["81988776652","8198872122"]
+
+};
+
+console.log(estudante.telefones[0]);
+
