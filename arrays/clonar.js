751b81658bac770426cc4d9231a28c38c2154c82
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-05 14:16:59 -0300

    feat: Adicionar clonagem de array de notas com spread operator

---
+++ arrays/clonar.js
@@ -0,0 +1,6 @@
+const notas = [7,7,8,9];
+const novaListaNotas = [...notas, 10];
+//spread operator pega todos os valores e abre o array de notas e adiciona uma nova nota(10)
+console.log(notas);
+
+console.log(novaListaNotas); 
