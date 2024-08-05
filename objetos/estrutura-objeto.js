8ee1857c9b4a43fa89f1334157ae310c3382fc90
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-05 14:41:14 -0300

    Refatorar código para adicionar comentários explicativos no arquivo de estrutura de objeto

--- objetos/estrutura-objeto.js
+++ objetos/estrutura-objeto.js
@@ -4,3 +4,11 @@ const estudante = {
   cpf: '12312312312',
   turma: 'JavaScript'
 }
+//notação de ponto para acessar o objeto para estudante.nome
+//objeto é sempre composto de dupla de chave e valor(string, numero, booleano)
+console.log(estudante.nome); // José Silva
+console.log(`o nome de estudante é ${estudante.nome}`);
+console.log(`os tres primeiros numeros do cpf são ${estudante.cpf.substring(0,3)}`);
+//0 e 3 pq exclui o ultimo numero
+
+
