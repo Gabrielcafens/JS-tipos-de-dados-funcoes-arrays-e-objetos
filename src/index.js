5eb876b1dde583a4d32248ac4d64e5e547b1aab0
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-06 13:54:01 -0300

    feat: regex  e Retirando palavras curtas

--- src/index.js
+++ src/index.js
@@ -18,15 +18,19 @@ function quebraEmParagrafos(texto) {
   console.log(contagem);
 }
 
-//transformando o texto em um array de palavras e contando a quantidade de vezes que cada palavra aparece
 
-
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
 function verificaPalavrasDuplicadas(texto) {
   const listaPalavras = texto.split(' ');
   const resultado = {};
   // objeto[propriedade] = valor;
   listaPalavras.forEach(palavra => {
-    resultado[palavra] = (resultado[palavra] || 0) + 1
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+    }
   })
   return resultado;
 }
