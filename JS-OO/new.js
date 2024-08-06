df6105d7fdab199cc8906d705515fce69f30fbd4
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-06 17:35:15 -0300

    função construtora

---
+++ JS-OO/new.js
@@ -0,0 +1,12 @@
+//função construtora é uma função que cria um objeto com propriedades e métodos
+function User (nome, email) {
+  this.nome = nome;
+  this.email = email;
+
+  this.exibirInfos = function() {
+    return `${this.nome}, ${this.email}`
+  }
+}
+//new serve para criar instancia de um objeto numa função construtora
+const novoUser = new User('Juliana', 'j@j.com')
+console.log(novoUser.exibirInfos());
