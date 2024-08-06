d63ac8f3692a0841d260415c76e0ef87415a097f
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-06 13:28:47 -0300

    feat: split

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 const fs = required('fs');
-
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2]; 
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-  console.log(texto);
+  quebraEmParagrafos(texto);
+  // verificaPalavrasDuplicadas(texto);
 })
+// criar um array com as palavras do texto
+// contador que vai checar se a palavra já foi contada até o final do array
+// montar um objeto com o resultado
+
+function quebraEmParagrafos(texto) {
+  const paragrafos = texto.toLowerCase().split('\n');
+  const contagem = paragrafos.map((paragrafo) => {
+    return verificaPalavrasDuplicadas(paragrafo);
+  })
+  console.log(contagem);
+}
+
+//transformando o texto em um array de palavras e contando a quantidade de vezes que cada palavra aparece
+
+
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  // objeto[propriedade] = valor;
+  listaPalavras.forEach(palavra => {
+    resultado[palavra] = (resultado[palavra] || 0) + 1
+  })
+  return resultado;
+}
